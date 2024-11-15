83693b9351c53c78cffbea5f05b89f44e92667ab
fifiinvtsr <149783209+fifiinvtsr@users.noreply.github.com>
2024-11-15 20:48:23 +0700

    mobile update

---
+++ dart_application_5/bin/dart_application_5.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_5/dart_application_5.dart' as dart_application_5;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_5.calculate()}!');
+}
